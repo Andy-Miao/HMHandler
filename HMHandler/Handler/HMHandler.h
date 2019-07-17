7fbf713655702c905df5d171788d27155ae296b0
Andyhu <871649716qq.com>
2019-07-17 23:57:19 +0800

    + Handler

--- HMHandler/Handler/HMHandler.h
+++ HMHandler/Handler/HMHandler.h
@@ -8,13 +8,42 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-@interface  : <#superclass#>
 
+@interface HMMessage : NSObject
+@property int what;
+@property int arg1;
+@property int arg2;
+@property (nonatomic, strong) NSObject *obj;
++ (id)msgWithWhat:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj;
+
+- (id)initWithWhat:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj;
+@end
+//MARK: 协议
+@protocol HMHandleDelegate <NSObject>
+@optional
+- (void)didHandlrMessage:(HMMessage *)msg;
 @end
 
+//MARK: HMHandler类
 @interface HMHandler : NSObject
 
+- (id)initWithDelegate:(id<HMHandleDelegate>)delegate;
+
+- (void)sendMessage:(int)what;
+- (void)sendMessage:(int)what delay:(int)delay;
+
+- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2;
+- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 delay:(int)delay;
+
+- (void)sendMessage:(int)what obj:(NSObject *)obj;
+- (void)sendMessage:(int)what obj:(NSObject *)obj delay:(int)delay;
+
+- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj;
+- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj delay:(int)delay;
+- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj waitUntilDone:(BOOL) waitUntilDone;
+
+- (void)removeMessages:(int)what;
+- (void)removeAllMessages;
 @end
 
-NS_ASSUME_NONNULL_END
+
--- HMHandler/Handler/HMHandler.m
+++ HMHandler/Handler/HMHandler.m
@@ -8,6 +8,59 @@
 
 #import "HMHandler.h"
 
+@interface HMMessage ()
+@property int delay;
+@end
+
+@implementation HMMessage
+
++ (id)msgWithWhat:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj {
+    HMMessage *msg = [[HMMessage alloc] initWithWhat:what arg1:arg1 arg2:arg2 obj:obj];
+    return msg;
+}
+
+- (id)initWithWhat:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj {
+    if (self = [super init]) {
+        self.what = what;
+        self.arg1 = arg1;
+        self.arg2 = arg2;
+        self.obj  = obj;
+    }
+    return self;
+}
+@end
+
+//MARK: HMHandler类
+@interface HMHandler ()
+@property (strong, nonatomic) id<HMHandleDelegate> delegate;
+@property (strong, nonatomic) NSMutableArray *msgList;
+@property (strong, nonatomic) NSThread *targetThread;
+@end
+
 @implementation HMHandler
 
+- (id)initWithDelegate:(id<HMHandleDelegate>)delegate {
+    if (self = [super init]) {
+        self.delegate = delegate;
+        self.msgList = [[NSMutableArray alloc]init];
+        self.targetThread = [NSThread currentThread];
+    }
+    return self;
+}
+
+//- (void)sendMessage:(int)what;
+//- (void)sendMessage:(int)what delay:(int)delay;
+//
+//- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2;
+//- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 delay:(int)delay;
+//
+//- (void)sendMessage:(int)what obj:(NSObject *)obj;
+//- (void)sendMessage:(int)what obj:(NSObject *)obj delay:(int)delay;
+//
+//- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj;
+//- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj delay:(int)delay;
+//- (void)sendMessage:(int)what arg1:(int)arg1 arg2:(int)arg2 obj:(NSObject *)obj waitUntilDone:(BOOL) waitUntilDone;
+//
+//- (void)removeMessages:(int)what;
+//- (void)removeAllMessages;
 @end
